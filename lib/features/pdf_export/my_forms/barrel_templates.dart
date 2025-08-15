ad833d83fe181396405eb667080dc4a3cb75154b
hmada2024 <alasuity881991@gmail.com>
2025-08-15 15:58:23 +0300

    1

--- lib/features/pdf_export/my_forms/barrel_templates.dart
+++
@@ -1,9 +0,0 @@
-// features/pdf_export/templates/barrel_templates.dart
-
-/// This file acts as a single entry point to all available PDF template builders.
-/// Any service that needs to generate a PDF can import this one file
-/// to get access to all templates, without needing to know their specific locations.
-library;
-
-export 'two_column_01/builder_template_01.dart';
-export 'two_column_02/builder_template_02.dart';
