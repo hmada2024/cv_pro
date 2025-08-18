3d83d3424e4f423931cb9192e91d3d6f9504dad5
hmada2024 <alasuity881991@gmail.com>
2025-08-18 06:10:59 +0300

    1

--- lib/features/3_cv_presentation/pdf_generation/main_builder/pdf_layout_builder.dart
+++ lib/features/3_cv_presentation/pdf_generation/main_builder/pdf_layout_builder.dart
@@ -4,7 +4,7 @@ import 'package:cv_pro/features/2_cv_editor/form/data/models/cv_data.dart';
 import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/black_blue_asymmetric/layout_black_blue_asymmetric.dart';
 import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/two_vertical_columns/layout_two_vertical_columns.dart';
 import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/blue_top_header/layout_blue_top_header.dart';
-import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/yellow_intersection/layout_yellow_intersection.dart';
+import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/intersection_designs/yellow_intersection/layout_yellow_intersection.dart';
 import 'package:cv_pro/features/3_cv_presentation/pdf_generation/core/pdf_template_layout_contract.dart';
 import 'package:pdf/widgets.dart' as pw;
 
