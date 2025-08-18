28e57654bc4be44141c701705461c4db2a79976f
hmada2024 <alasuity881991@gmail.com>
2025-08-18 04:23:15 +0300

    1

--- lib/features/3_cv_presentation/pdf_generation/main_builder/pdf_layout_builder.dart
+++ lib/features/3_cv_presentation/pdf_generation/main_builder/pdf_layout_builder.dart
@@ -1,9 +1,9 @@
 // lib/features/3_cv_presentation/pdf_generation/layout/pdf_layout_builder.dart
 import 'dart:typed_data';
 import 'package:cv_pro/features/2_cv_editor/form/data/models/cv_data.dart';
-import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/two_vertical_columns/classic_template_layout.dart';
-import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/blue_top_header/modern_template_layout.dart';
-import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/yellow_intersection/yellow_template_layout.dart';
+import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/two_vertical_columns/layout_two_vertical_columns.dart';
+import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/blue_top_header/layout_blue_top_header.dart';
+import 'package:cv_pro/features/3_cv_presentation/pdf_generation/cv_designs/yellow_intersection/layout_yellow_intersection.dart';
 import 'package:cv_pro/features/3_cv_presentation/pdf_generation/core/pdf_template_layout_contract.dart'; // استيراد العقد
 import 'package:pdf/widgets.dart' as pw;
 
